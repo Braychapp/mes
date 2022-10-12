dd8d57b57ec6e01fd38008dfaf446a6efa4f0d97
brayden chapple <bchapple8815@conestogac.on.ca>
2022-10-11 21:17:12 -0400

    first version of the code is finished, I don't expect it to work but debugging and revising starts now

--- bc_asm.s
+++ bc_asm.s
@@ -89,4 +89,73 @@ end_loop:
     bx lr
 
 
+
+
+
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+.code 16 @ This directive selects the instruction set being generated.
+@ The value 16 selects Thumb, with the value 32 selecting ARM.
+.text @ Tell the assembler that the upcoming section is to be considered
+@ assembly language instructions - Code section (text -> ROM)
+@@ Function Header Block
+.align 2 @ Code alignment - 2^n alignment (n=2)
+@ This causes the assembler to use 4 byte alignment
+.syntax unified @ Sets the instruction set to the new unified ARM + THUMB
+@ instructions. The default is divided (separate instruction sets)
+.global bc_led_demo_a2 @ Make the symbol name for the function visible to the linker
+.code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func @ Specifies that the following symbol is the name of a THUMB
+@ encoded function. Necessary for interlinking between ARM and THUMB code.
+.type bc_led_demo_a2, %function @ Declares that the symbol is a function (not strictly required)
+@ Function Declaration : int bc_led_demo_a2(int x, int y)
+
+bc_led_demo_a2:
+@r0 is count and r1 is delay
+push {r0-r7, lr} @pushing all available registers because I don't know what I'll end up using
+
+bl led_delay
+
+led_delay:
+mov r2, #0 @r2 is used to see if an led is on or off, if it's an even number the led should be off and if it's odd it should be on
+mov r3, r1 @moving the delay value to r4
+mov r4, r1 @moving the delay value to r4 for use later when resetting the loop
+mov r5, r0 @moving count into r5
+mov r6, #0 @r6 is going to keep track of how many times we have looped
+mov r7, #0 @r7 is going to hold which led is supposed to be toggled
+led_1oop: @looping r5 times
+    subs r3, r3, #1
+    bge led_1oop
+    @loop over
+@if r6 is greater than r5
+add r2, r2, #1
+cmp r6, r5 @comparing r6 to r5 to see how many times the loop is supposed to go through
+bgt else1 @else statement in C
+bl BSP_LED_Toggle @calling BSP_LED_Toggle
+mov r3, r4 @moving the delay back into r3 for another loop
+@need to put an if statement inside an if statement here
+cmp r2, #2
+bge else2
+bl led_1oop @if the led is toggled on and hasnt been turned off its going to loop again
+
+@used for switching which led is toggled
+else2:
+add r7, r7, #1 @change which led is being toggled
+mov r2, #0 @move the status of the LED back to being off
+bl led_1oop @going back to loop again
+
+@used for adding to the total times that the program has looped
+else1:
+add r6, r6, #1 @the program has officially looped and r6 needs to be updated to reflect that
+@need to put an if statement to check if the count is the same as the one that the user entered
+cmp r6, r5
+beq exit
+bl led_1oop @going back to the loop
+
+exit:
+pop {r0-r7, lr} @popping off everything
+bx lr @ Return (Branch eXchange) to the address in the link register (lr
+
+.size bc_led_demo_a2, .-bc_led_demo_a2 @@ - symbol size (not strictly required, but makes the debugger happy)
 .end
--- bc_hook.c
+++ bc_hook.c
@@ -31,34 +31,30 @@ delay = 0xFFFFFF;
 //r2 = 0xFFFFFF
 //r3 = 20
 //r3 needs to be 20 because to toggle on the LED is one and to turn it off is also 1 so each on / off counts for 2
-printf("add_test returned: %d\nDelay:", add_test(98, 87, delay, 20));
+printf("add_test returned: %d\n", add_test(98, 87, delay, 20));
 }
-ADD_CMD("add", AddTest,"Test the new add function");
+ADD_CMD("ads", AddTest,"Test the new add function");
+
+
+
+
 int bc_led_demo_a2(int count, int delay);
 void _bc_A2(int action)
 {
 if(action==CMD_SHORT_HELP) return;
 if(action==CMD_LONG_HELP) {
-printf("Addition Test\n\n"
-"This command tests new addition function\n"
+printf("LED DEMO\n\n"
+"This command tests that I know hwo to turn on all the LEDS\n"
 );
 return;
 }
-//creating a variable of type uint32_t called delay and a regular int called fetch_status
-uint32_t delay;
+uint32_t user_input;
 int fetch_status;
-fetch_status = fetch_uint32_arg(&delay);
+fetch_status = fetch_uint32_arg(&user_input);
 if(fetch_status) {
-// Use a default delay value
-delay = 0xFFFFFF;
+// Use a default value
+user_input = 0xFFFFFF;
 }
-// When we call our function, pass the delay value.
-// printf(“<<< here is where we call add_test – can you add a third parameter? >>>”);
-//r0 = 98
-//r1 = 87
-//r2 = 0xFFFFFF
-//r3 = 20
-//r3 needs to be 20 because to toggle on the LED is one and to turn it off is also 1 so each on / off counts for 2
-printf("add_test returned: %d\nDelay:", add_test(98, 87, delay, 20));
+printf("bc_led_demo_a2 has finished, if 0 it has worked correctly: %d", bc_led_demo_a2(1, 0xFFFFFF)); //0 is the counter and delay is the delay from the user
 }
-ADD_CMD("led", _bc_A2, "Test the _bc_A2 function");
+ADD_CMD("add", _bc_A2, "Test the _bc_A2 function");
