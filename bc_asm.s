715c1a539c8e901a9c151c3997c93cc91b639c36
brayden chapple <bchapple8815@conestogac.on.ca>
2022-11-22 22:04:16 -0500

    created an empty function

--- bc_asm.s
+++ bc_asm.s
@@ -621,6 +621,36 @@ accel_test:
 @ Assembly file ended by single .end directive on its own line
 
 
+@ Test code for my own new function called from C
+@ This is a comment. Anything after an @ symbol is ignored.
+@@ This is also a comment. Some people use double @@ symbols.
+.code 16 @ This directive selects the instruction set being generated.
+@ The value 16 selects Thumb, with the value 32 selecting ARM.
+.text @ Tell the assembler that the upcoming section is to be considered
+@ assembly language instructions - Code section (text -> ROM)
+@@ Function Header Block
+.align 2 @ Code alignment - 2^n alignment (n=2)
+@ This causes the assembler to use 4 byte alignment
+.syntax unified @ Sets the instruction set to the new unified ARM + THUMB
+@ instructions. The default is divided (separate instruction sets)
+.global bc_tilt @ Make the symbol name for the function visible to the linker
+.code 16 @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+.thumb_func @ Specifies that the following symbol is the name of a THUMB
+@ encoded function. Necessary for interlinking between ARM and THUMB code.
+.type bc_tilt, %function @ Declares that the symbol is a function (not strictly required)
+
+bc_tilt:
+@start of function
+    push {lr}
+
+    pop {lr}
+    bx lr
+
+
+.size bc_tile, .-bc_tilt @@ - symbol size (not strictly required, but makes the debugger happy)
+
+
+
 @function to convert the accel values into whatever light  on the board
 accel_to_LED:
  @inputs arrive in r0 and r1
